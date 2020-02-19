a9100c21525ba7e580a26ca682e7eb85d43d34bb
kostya9 <kostya9@gmail.com>
2020-02-19 09:10:23 +0100

    Add tests

--- src/routes/testRoute.ts
+++ src/routes/testRoute.ts
@@ -5,17 +5,43 @@ import * as uuid from 'uuid'
 const dynamo = new AWS.DynamoDB()
 
 // Define our routes, independent from the API Gateway itself.
-export async function testRouteHandler(
+export async function testRouteCreate(
         event: awsx.apigateway.Request): Promise<awsx.apigateway.Response> {
+            const newId = uuid()
     return dynamo.putItem({
         TableName: 'test-stuff',
-        Item: {Id: {S: uuid()}}
+        Item: {Id: {S: newId}}
     })
     .promise()
     .then((r) => {
         return {
             statusCode: 200,
-            body: "Hello, API Gateway!",
+            body: JSON.stringify({id: newId}),
         };
     })
+}
+
+// Define our routes, independent from the API Gateway itself.
+export async function testRouteGet(
+    event: awsx.apigateway.Request): Promise<awsx.apigateway.Response> {
+        
+        const body = JSON.parse(event.body || '')
+return dynamo.getItem({
+    TableName: 'test-stuff',
+    Key: {"Id": {"S": body.id.toString()}}
+})
+.promise()
+.then((r) => {
+    if(r.Item) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({id: r.Item.Id.S}),
+        };
+    }
+
+    return {
+        statusCode: 404,
+        body: 'Not Found'
+    }
+})
 }
--- tests/testHandler/test.ts
+++ tests/testHandler/test.ts
@@ -1,14 +1,34 @@
 import { expect } from 'chai'
 import './../configTestEnvironment'
-import {testRouteHandler} from './../../src/routes/testRoute'
+import {testRouteCreate, testRouteGet} from './../../src/routes/testRoute'
 import {apiGatewayWithBody} from './../testApiGatewayRequest'
 
 
-describe('Hello function', () => {
-    it('should return hello world', () => {
-      const result = testRouteHandler( apiGatewayWithBody({d: 'd'}) )
+describe('testRoute function', () => {
+    it('should return new item on create', () => {
+      const result = testRouteCreate( apiGatewayWithBody({}) )
       return result.then((r) => {
           expect(r.statusCode).to.equal(200)
+          expect(JSON.parse(r.body).id).to.not.be.empty
+      })
+    })
+
+    it('should return new item on get after create', () => {
+      const result = testRouteCreate( apiGatewayWithBody({}) )
+      return result.then((r) => {
+          const id = JSON.parse(r.body).id
+          const result = testRouteGet( apiGatewayWithBody({id: id}))
+          return result.then((r) => {
+            expect(r.statusCode).to.equal(200)
+            expect(JSON.parse(r.body).id).to.be.equal(id)
+          })
+      })
+    })
+
+    it('should return 404 in random id', () => {
+      const result = testRouteGet( apiGatewayWithBody({id: Math.random()}) )
+      return result.then((r) => {
+          expect(r.statusCode).to.be.equal(404)
       })
     })
   })
