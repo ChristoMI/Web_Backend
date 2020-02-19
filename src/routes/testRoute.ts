d09630fb07f72ba4172a3e6b5bdef5c604b5f0a0
kostya9 <kostya9@gmail.com>
2020-02-19 09:12:39 +0100

    Format stuff

--- src/routes/testRoute.ts
+++ src/routes/testRoute.ts
@@ -6,42 +6,42 @@ const dynamo = new AWS.DynamoDB()
 
 // Define our routes, independent from the API Gateway itself.
 export async function testRouteCreate(
-        event: awsx.apigateway.Request): Promise<awsx.apigateway.Response> {
-            const newId = uuid()
+    event: awsx.apigateway.Request): Promise<awsx.apigateway.Response> {
+    const newId = uuid()
     return dynamo.putItem({
         TableName: 'test-stuff',
-        Item: {Id: {S: newId}}
+        Item: { Id: { S: newId } }
     })
-    .promise()
-    .then((r) => {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({id: newId}),
-        };
-    })
+        .promise()
+        .then((r) => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ id: newId }),
+            };
+        })
 }
 
 // Define our routes, independent from the API Gateway itself.
 export async function testRouteGet(
     event: awsx.apigateway.Request): Promise<awsx.apigateway.Response> {
-        
-        const body = JSON.parse(event.body || '')
-return dynamo.getItem({
-    TableName: 'test-stuff',
-    Key: {"Id": {"S": body.id.toString()}}
-})
-.promise()
-.then((r) => {
-    if(r.Item) {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({id: r.Item.Id.S}),
-        };
-    }
 
-    return {
-        statusCode: 404,
-        body: 'Not Found'
-    }
-})
+    const body = JSON.parse(event.body || '')
+    return dynamo.getItem({
+        TableName: 'test-stuff',
+        Key: { "Id": { "S": body.id.toString() } }
+    })
+        .promise()
+        .then((r) => {
+            if (r.Item) {
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ id: r.Item.Id.S }),
+                };
+            }
+
+            return {
+                statusCode: 404,
+                body: 'Not Found'
+            }
+        })
 }
