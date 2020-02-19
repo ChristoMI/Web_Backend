ad58a90786bce52018ef7f9592145be8bfdf8bf1
kostya9 <kostya9@gmail.com>
2020-02-19 04:29:56 +0100

    Limit concurrent executions

--- index.ts
+++ index.ts
@@ -1,14 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
-import {testRouteHandler} from "./routes/testRoute";
+import { testRouteHandler } from "./routes/testRoute";
 
 const stackConfig = new pulumi.Config("site");
 const domain = stackConfig.require("domain");
 const certArn = stackConfig.require("certificateArn");
 
 const api = new awsx.apigateway.API("booking-api", {
-    routes: [{ path: "/test", method: "GET", eventHandler: testRouteHandler }]
+    routes: [{
+        path: "/test", 
+        method: "GET", 
+        eventHandler: new aws.lambda.CallbackFunction("testRouteHandler", {
+            callback: testRouteHandler,
+            reservedConcurrentExecutions: 1
+        })
+    }],
 })
 const domainName = new aws.apigateway.DomainName("booking-domain", {
     domainName: domain,
