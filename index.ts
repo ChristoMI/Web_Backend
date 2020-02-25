11a657e62a88b3f6efdcde4b34165db00ee35c31
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2020-02-26 00:50:31 +0200

    Feature/comments (#11)
    * Added eslint config and aliases
    
    * added comments
    
    * Added eslint config and aliases
    
    * added comments
    
    * changed ml server url
    
    * fixed comments
    
    * property check added and new tests added
    
    * added ts type for dynamo

--- index.ts
+++ index.ts
@@ -1,151 +1,193 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
-import { testRouteGet, testRouteCreate } from "./src/routes/testRoute";
-import { propertyInsert, propertyUpdate, propertyGetById, propertiesGet, STATIC_BUCKET_ENV_KEY, STATIC_DOMAIN_ENV_KEY } from "./src/routes/propertiesRoute";
+import 'module-alias/register'; // for alias
 
-import './infrastructure/dynamodb'
-import { staticBucket, staticDomain } from './infrastructure/staticContent'
-import { Route } from "@pulumi/awsx/apigateway/api";
+import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
+import { testRouteGet, testRouteCreate } from './src/routes/testRoute';
 
-const stackConfig = new pulumi.Config("site");
-const domain = stackConfig.require("domain");
-const certArn = stackConfig.require("certificateArn");
+import {
+  propertyInsert, propertyUpdate, propertyGetById, propertiesGet, STATIC_BUCKET_ENV_KEY, STATIC_DOMAIN_ENV_KEY,
+} from './src/routes/propertiesRoute';
+import * as commentsRoutes from '$src/routes/comments';
+
+import './infrastructure/dynamodb';
+import { staticBucket, staticDomain } from './infrastructure/staticContent';
+import { Route } from '@pulumi/awsx/apigateway/api';
+
+
+const stackConfig = new pulumi.Config('site');
+const domain = stackConfig.require('domain');
+const certArn = stackConfig.require('certificateArn');
 
 const variables = {
-    [STATIC_BUCKET_ENV_KEY]: staticBucket,
-    [STATIC_DOMAIN_ENV_KEY]: staticDomain
-}
+  [STATIC_BUCKET_ENV_KEY]: staticBucket,
+  [STATIC_DOMAIN_ENV_KEY]: staticDomain,
+  MLServerUrl: 'http://54.214.143.113:80',
+};
 
 const environment = {
-    variables
-}
+  variables,
+};
 
 let routes: Route[] = [{
-    path: "/test/{id}",
-    method: "GET",
-    requiredParameters: [
-        { in: "path", name: "id" }
-    ],
-    eventHandler: new aws.lambda.CallbackFunction("testRouteGet", {
-        callbackFactory: testRouteGet,
-        reservedConcurrentExecutions: 1,
-        tracingConfig: {
-            mode: 'Active'
-        }
-    })
+  path: '/test/{id}',
+  method: 'GET',
+  requiredParameters: [
+    { in: 'path', name: 'id' },
+  ],
+  eventHandler: new aws.lambda.CallbackFunction('testRouteGet', {
+    callbackFactory: testRouteGet,
+    reservedConcurrentExecutions: 1,
+    tracingConfig: {
+      mode: 'Active',
+    },
+  }),
 },
 {
-    path: "/test",
-    method: "POST",
-    eventHandler: new aws.lambda.CallbackFunction("testRouteCreate", {
-        callbackFactory: testRouteCreate,
-        reservedConcurrentExecutions: 1,
-        tracingConfig: {
-            mode: 'Active'
-        }
-    })
+  path: '/test',
+  method: 'POST',
+  eventHandler: new aws.lambda.CallbackFunction('testRouteCreate', {
+    callbackFactory: testRouteCreate,
+    reservedConcurrentExecutions: 1,
+    tracingConfig: {
+      mode: 'Active',
+    },
+  }),
 },
 {
-    path: "/properties",
-    method: "GET",
-    eventHandler: new aws.lambda.CallbackFunction("propertiesGet", {
-        callbackFactory: propertiesGet,
-        reservedConcurrentExecutions: 1,
-        tracingConfig: {
-            mode: 'Active'
-        },
-        environment
-    })
+  path: '/properties',
+  method: 'GET',
+  eventHandler: new aws.lambda.CallbackFunction('propertiesGet', {
+    callbackFactory: propertiesGet,
+    reservedConcurrentExecutions: 1,
+    tracingConfig: {
+      mode: 'Active',
+    },
+    environment,
+  }),
 },
 {
-    path: "/properties/{id}",
-    method: "GET",
-    requiredParameters: [
-        { in: "path", name: "id" }
-    ],
-    eventHandler: new aws.lambda.CallbackFunction("propertyGetById", {
-        callbackFactory: propertyGetById,
-        reservedConcurrentExecutions: 1,
-        tracingConfig: {
-            mode: 'Active'
-        },
-        environment
-    })
+  path: '/properties/{id}',
+  method: 'GET',
+  requiredParameters: [
+    { in: 'path', name: 'id' },
+  ],
+  eventHandler: new aws.lambda.CallbackFunction('propertyGetById', {
+    callbackFactory: propertyGetById,
+    reservedConcurrentExecutions: 1,
+    tracingConfig: {
+      mode: 'Active',
+    },
+    environment,
+  }),
 },
 {
-    path: "/properties",
-    method: "POST",
-    eventHandler: new aws.lambda.CallbackFunction("propertyInsert", {
-        callbackFactory: propertyInsert,
-        reservedConcurrentExecutions: 1,
-        tracingConfig: {
-            mode: 'Active'
-        },
-        environment
-    })
+  path: '/properties',
+  method: 'POST',
+  eventHandler: new aws.lambda.CallbackFunction('propertyInsert', {
+    callbackFactory: propertyInsert,
+    reservedConcurrentExecutions: 1,
+    tracingConfig: {
+      mode: 'Active',
+    },
+    environment,
+  }),
 },
 {
-    path: "/properties/{id}",
-    method: "PUT",
-    requiredParameters: [
-        { in: "path", name: "id" }
-    ],
-    eventHandler: new aws.lambda.CallbackFunction("propertyUpdate", {
-        callbackFactory: propertyUpdate,
-        reservedConcurrentExecutions: 1,
-        tracingConfig: {
-            mode: 'Active'
-        },
-        environment
-    })
-}]
+  path: '/properties/{id}',
+  method: 'PUT',
+  requiredParameters: [
+    { in: 'path', name: 'id' },
+  ],
+  eventHandler: new aws.lambda.CallbackFunction('propertyUpdate', {
+    callbackFactory: propertyUpdate,
+    reservedConcurrentExecutions: 1,
+    tracingConfig: {
+      mode: 'Active',
+    },
+    environment,
+  }),
+},
+{
+  path: '/properties/{id}/comments',
+  method: 'GET',
+  requiredParameters: [{
+    in: 'path',
+    name: 'id',
+  }],
+  eventHandler: new aws.lambda.CallbackFunction('getCommentsByPropertyId', {
+    callbackFactory: commentsRoutes.getCommentsByPropertyId,
+    reservedConcurrentExecutions: 1,
+    tracingConfig: {
+      mode: 'Active',
+    },
+    environment,
+  }),
+},
+{
+  path: '/properties/{id}/comments',
+  method: 'POST',
+  requiredParameters: [{
+    in: 'path',
+    name: 'id',
+  }],
+  eventHandler: new aws.lambda.CallbackFunction('createPropertyComment', {
+    callbackFactory: commentsRoutes.createPropertyComment,
+    reservedConcurrentExecutions: 1,
+    tracingConfig: {
+      mode: 'Active',
+    },
+    environment,
+  }),
+},
+];
 
 function addCors(routes: Route[]) {
-    const corslambda = new aws.lambda.CallbackFunction<awsx.apigateway.Request, awsx.apigateway.Response>("corsLambda", {
-        callback: async (e) => ({
-            statusCode: 200,
-            body: '',
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Methods": "*"
-            }
-        }),
-        reservedConcurrentExecutions: 1
-    })
+  const corslambda = new aws.lambda.CallbackFunction<awsx.apigateway.Request, awsx.apigateway.Response>('corsLambda', {
+    callback: async (e) => ({
+      statusCode: 200,
+      body: '',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': '*',
+      },
+    }),
+    reservedConcurrentExecutions: 1,
+  });
 
-    const newRoutes = Array.from(routes)
-    
-    for(const route of routes) {
-        newRoutes.push({
-            method: "OPTIONS",
-            path: route.path,
-            eventHandler: corslambda
-        })
-    }
+  const newRoutes = Array.from(routes);
 
-    return newRoutes
+  for (const route of routes) {
+    newRoutes.push({
+      method: 'OPTIONS',
+      path: route.path,
+      eventHandler: corslambda,
+    });
+  }
+
+  return newRoutes;
 }
 
-routes = addCors(routes)
+routes = addCors(routes);
 
-const api = new awsx.apigateway.API("booking-api", {
-    routes: routes,
-    stageArgs: {
-        xrayTracingEnabled: true
-    }
-})
-
-const domainName = new aws.apigateway.DomainName("booking-domain", {
-    domainName: domain,
-    certificateArn: certArn,
-})
-const domainMapping = new aws.apigateway.BasePathMapping("booking-domain-mapping", {
-    restApi: api.restAPI,
-    domainName: domainName.domainName,
-    stageName: api.stage.stageName,
+const api = new awsx.apigateway.API('booking-api', {
+  routes,
+  stageArgs: {
+    xrayTracingEnabled: true,
+  },
 });
 
-export const url = api.url
-export const staticBucketName = staticBucket
+const domainName = new aws.apigateway.DomainName('booking-domain', {
+  domainName: domain,
+  certificateArn: certArn,
+});
+
+const domainMapping = new aws.apigateway.BasePathMapping('booking-domain-mapping', {
+  restApi: api.restAPI,
+  domainName: domainName.domainName,
+  stageName: api.stage.stageName,
+});
+
+export const url = api.url;
+export const staticBucketName = staticBucket;
---
+++ src/apiGatewayUtilities/index.ts
@@ -0,0 +1,34 @@
+import { apigateway } from "@pulumi/awsx";
+import { DynamoDB } from 'aws-sdk';
+
+export const marshall = DynamoDB.Converter.marshall;
+export const unmarshall = DynamoDB.Converter.unmarshall;
+
+export function unmarshalls(items: DynamoDB.Types.ItemList) {
+  return items.map((item) => unmarshall(item));
+}
+
+export function parseBody(event: apigateway.Request): { [key: string]: any } {
+  const body = event.body || '{}';
+
+  if (event.isBase64Encoded) {
+    const buffer = new Buffer(body, 'base64');
+    return JSON.parse(buffer.toString());
+  }
+
+  return JSON.parse(body);
+}
+
+export function buildApiResponse(
+  statusCode: number,
+  payload: { [key: string]: any },
+): apigateway.Response {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(payload),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  };
+}
