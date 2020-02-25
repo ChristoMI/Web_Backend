56c957ab4b64a2d6a417ee74f45e400370d8aa23
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2020-02-25 17:22:12 +0200

    added comments

--- infrastructure/dynamodb.ts
+++ infrastructure/dynamodb.ts
@@ -1,27 +1,48 @@
-import * as aws from "@pulumi/aws";
+import * as aws from '@pulumi/aws';
 
 const dynamo = new aws.dynamodb.Table('test-stuff', {
-    attributes: [
-        { name: 'Id', type: 'S'}
-    ],
-    readCapacity: 1,
-    writeCapacity: 1,
-    name: 'test-stuff',
+  attributes: [{
+    name: 'Id',
+    type: 'S',
+  }],
+  readCapacity: 1,
+  writeCapacity: 1,
+  name: 'test-stuff',
+  hashKey: 'Id',
+  globalSecondaryIndexes: [{
     hashKey: 'Id',
-    globalSecondaryIndexes: [
-        {hashKey: 'Id', name: 'test-global-secondary', readCapacity: 1, writeCapacity: 1, projectionType: 'ALL'}
-    ]
-})
-
-const properties = new aws.dynamodb.Table('properties', {
-    attributes: [
-        { name: 'id', type: 'S'}
-    ],
+    name: 'test-global-secondary',
     readCapacity: 1,
     writeCapacity: 1,
-    name: 'properties',
-    hashKey: 'id'
-})
+    projectionType: 'ALL',
+  }],
+});
 
 export const testStuff = dynamo;
+
+const properties = new aws.dynamodb.Table('properties', {
+  attributes: [
+    { name: 'id', type: 'S' },
+  ],
+  readCapacity: 1,
+  writeCapacity: 1,
+  name: 'properties',
+  hashKey: 'id',
+});
+
 export const propertiesTable = properties;
+
+export const comment = new aws.dynamodb.Table('comment', {
+  name: 'comment',
+  rangeKey: 'id',
+  hashKey: 'propertyId',
+  attributes: [{
+    name: 'id',
+    type: 'S',
+  }, {
+    name: 'propertyId',
+    type: 'S',
+  }],
+  readCapacity: 1,
+  writeCapacity: 1,
+});
