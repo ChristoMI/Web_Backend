7743bc51181005f82d5df09eb03aa11428bbf115
Kostyantyn Sharovarsky <kostya9@gmail.com>
2020-02-25 14:48:47 +0200

    Add cors

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import { propertyInsert, propertyUpdate, propertyGetById, propertiesGet, STATIC_
 
 import './infrastructure/dynamodb'
 import { staticBucket, staticDomain } from './infrastructure/staticContent'
+import { Route } from "@pulumi/awsx/apigateway/api";
 
 const stackConfig = new pulumi.Config("site");
 const domain = stackConfig.require("domain");
@@ -20,86 +21,115 @@ const environment = {
     variables
 }
 
+const routes: Route[] = [{
+    path: "/test/{id}",
+    method: "GET",
+    requiredParameters: [
+        { in: "path", name: "id" }
+    ],
+    eventHandler: new aws.lambda.CallbackFunction("testRouteGet", {
+        callbackFactory: testRouteGet,
+        reservedConcurrentExecutions: 1,
+        tracingConfig: {
+            mode: 'Active'
+        }
+    })
+},
+{
+    path: "/test",
+    method: "POST",
+    eventHandler: new aws.lambda.CallbackFunction("testRouteCreate", {
+        callbackFactory: testRouteCreate,
+        reservedConcurrentExecutions: 1,
+        tracingConfig: {
+            mode: 'Active'
+        }
+    })
+},
+{
+    path: "/properties",
+    method: "GET",
+    eventHandler: new aws.lambda.CallbackFunction("propertiesGet", {
+        callbackFactory: propertiesGet,
+        reservedConcurrentExecutions: 1,
+        tracingConfig: {
+            mode: 'Active'
+        },
+        environment
+    })
+},
+{
+    path: "/properties/{id}",
+    method: "GET",
+    requiredParameters: [
+        { in: "path", name: "id" }
+    ],
+    eventHandler: new aws.lambda.CallbackFunction("propertyGetById", {
+        callbackFactory: propertyGetById,
+        reservedConcurrentExecutions: 1,
+        tracingConfig: {
+            mode: 'Active'
+        },
+        environment
+    })
+},
+{
+    path: "/properties",
+    method: "POST",
+    eventHandler: new aws.lambda.CallbackFunction("propertyInsert", {
+        callbackFactory: propertyInsert,
+        reservedConcurrentExecutions: 1,
+        tracingConfig: {
+            mode: 'Active'
+        },
+        environment
+    })
+},
+{
+    path: "/properties/{id}",
+    method: "PUT",
+    requiredParameters: [
+        { in: "path", name: "id" }
+    ],
+    eventHandler: new aws.lambda.CallbackFunction("propertyUpdate", {
+        callbackFactory: propertyUpdate,
+        reservedConcurrentExecutions: 1,
+        tracingConfig: {
+            mode: 'Active'
+        },
+        environment
+    })
+}]
+
+function addCors(routes: Route[]) {
+    const corslambda = new aws.lambda.CallbackFunction<awsx.apigateway.Request, awsx.apigateway.Response>("propertyUpdate", {
+        callback: async (e) => ({
+            statusCode: 200,
+            body: '',
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Methods": "*"
+            }
+        }),
+        reservedConcurrentExecutions: 1,
+        tracingConfig: {
+            mode: 'Active'
+        }
+    })
+
+    const newRoutes = Array.from(routes)
+    for(const route of routes) {
+        newRoutes.push({
+            method: "OPTIONS",
+            path: route.path,
+            eventHandler: corslambda
+        })
+    }
+}
+
 const api = new awsx.apigateway.API("booking-api", {
-    routes: [{
-        path: "/test/{id}",
-        method: "GET",
-        requiredParameters: [
-            { in: "path", name: "id" }
-        ],
-        eventHandler: new aws.lambda.CallbackFunction("testRouteGet", {
-            callbackFactory: testRouteGet,
-            reservedConcurrentExecutions: 1,
-            tracingConfig: {
-                mode: 'Active'
-            }
-        })
-    },
-    {
-        path: "/test",
-        method: "POST",
-        eventHandler: new aws.lambda.CallbackFunction("testRouteCreate", {
-            callbackFactory: testRouteCreate,
-            reservedConcurrentExecutions: 1,
-            tracingConfig: {
-                mode: 'Active'
-            }
-        })
-    },
-    {
-        path: "/properties",
-        method: "GET",
-        eventHandler: new aws.lambda.CallbackFunction("propertiesGet", {
-            callbackFactory: propertiesGet,
-            reservedConcurrentExecutions: 1,
-            tracingConfig: {
-                mode: 'Active'
-            },
-            environment
-        })
-    },
-    {
-        path: "/properties/{id}",
-        method: "GET",
-        requiredParameters: [
-            { in: "path", name: "id" }
-        ],
-        eventHandler: new aws.lambda.CallbackFunction("propertyGetById", {
-            callbackFactory: propertyGetById,
-            reservedConcurrentExecutions: 1,
-            tracingConfig: {
-                mode: 'Active'
-            },
-            environment
-        })
-    },
-    {
-        path: "/properties",
-        method: "POST",
-        eventHandler: new aws.lambda.CallbackFunction("propertyInsert", {
-            callbackFactory: propertyInsert,
-            reservedConcurrentExecutions: 1,
-            tracingConfig: {
-                mode: 'Active'
-            },
-            environment
-        })
-    },
-    {
-        path: "/properties/{id}",
-        method: "PUT",
-        requiredParameters: [
-            { in: "path", name: "id" }
-        ],
-        eventHandler: new aws.lambda.CallbackFunction("propertyUpdate", {
-            callbackFactory: propertyUpdate,
-            reservedConcurrentExecutions: 1,
-            tracingConfig: {
-                mode: 'Active'
-            },
-            environment
-        })
-    }],
+    routes: routes,
     stageArgs: {
         xrayTracingEnabled: true
     }
