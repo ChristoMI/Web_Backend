c85f01e802f6402494dfbe90fe1604c8116176df
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2020-03-14 14:56:13 +0100

    Add support for swagger to client

--- index.ts
+++ index.ts
@@ -57,12 +57,12 @@ const hostsUserPool = new aws.cognito.UserPool('booking-user-pool-hosts', {
 });
 
 const customersUserPoolClient = new aws.cognito.UserPoolClient('booking-user-pool-client-customers', {
-  allowedOauthFlows: ['code'],
+  allowedOauthFlows: ['code', 'implicit'],
   allowedOauthFlowsUserPoolClient: true,
   allowedOauthScopes: ['phone', 'email', 'openid'],
-  callbackUrls: ['http://localhost:3000'],
+  callbackUrls: ['http://localhost:3000', 'https://landing.booking.knine.xyz/swagger'],
   generateSecret: false,
-  supportedIdentityProviders: ['COGNITO'],
+  supportedIdentityProviders: ['COGNITO', 'Google'],
   userPoolId: customersUserPool.id,
 });
 
